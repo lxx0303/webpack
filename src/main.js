7a3579782deaf7a51b45cc6dd2aecf0b28cb5c60
lxx0303 <2434921421@qq.com>
2022-06-17 21:12:56 +0800

    更新3

--- src/main.js
+++ src/main.js
@@ -10,3 +10,35 @@ import { tab } from './tab.js';
 // yarn build  或者 yarn run build
 tab();
 marquee();
+
+// 引用css
+import './styles/index.css'
+
+// 引用less
+import './styles/index.less'
+
+// 1.引入图片
+import gifSrc from './assets/1.gif';
+import pngSrc from './assets/logo_small.png';
+
+// 2.创建图片节点
+const gif = document.createElement('img');
+const png = document.createElement('img');
+
+// 3.给src赋值
+gif.src = gifSrc;
+png.src = pngSrc;
+
+// 4.
+document.body.appendChild(gif);
+document.body.appendChild(png);
+
+
+// 字体图标
+import './assets/fonts/iconfont.css'
+
+// 高版本js -->  低版本js
+const fn = () => {
+    console.log('你好');
+}
+console.log(fn);
--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,45 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: join(__dirname, 'public/index.html')
         })
-    ]
+    ],
+    devServer: {
+        open: true,
+        port: 30000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.less$/i,
+                // 从后往前解析
+                use: ["style-loader", "css-loader", "less-loader"]
+            },
+            {
+                test: /\.(png|gif|jpeg)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        // 字节
+                        maxSize: 8 * 1024
+                    }
+                },
+                generator: {
+                    filename: 'images/[name].[hash:6][ext]'
+                }
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font/[name].[hash:6][ext]'
+                }
+            }, {
+                test: /\.js$/i,
+                use: ["babel-loader"]
+            }
+        ]
+    }
 }
