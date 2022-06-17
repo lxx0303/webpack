6ab0ca55f01344853d488f1501922948a416e10d
lxx0303 <2434921421@qq.com>
2022-06-17 20:18:44 +0800

    更新2

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { marquee } from './marquee.js';
+import { tab } from './tab.js';
+
+// 1. yarn init
+// 安装yarn  npm i yarn -g
+// 2.yarn add webpack webpack-cli -D
+// -webpack核心包 webpack-cli/command line interce
+// 3.scripts
+// 4.执行脚本 yarn run <scriptname>
+// yarn build  或者 yarn run build
+tab();
+marquee();
